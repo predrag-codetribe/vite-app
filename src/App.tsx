c506b6de1ab3924b59f0e53966be8bc8c6fb6906
Predrag Nikolic <predrag.nikolic@codetri.be>
2022-12-23 15:25:37 +0100

    feat: add dashboard component

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,41 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Outlet } from 'react-router-dom'
 import ActiveLink from './router/ActiveLink'
 import { Page } from './router/Page'
 
 const HomePage = React.lazy(() => import('./pages/HomePage/HomePage'))
 const AboutPage = React.lazy(() => import('./pages/AboutPage/AboutPage'))
 
-const Links = () => {
+const Dashboard = () => {
     const { t } = useTranslation()
-    return <div className='flex gap-1'>
-        <ActiveLink to='/'
-            className='p-1'
-            activeClassName='border-b'>{t('home_page.home')}</ActiveLink>
+    return <div>
+        <header className='flex gap-1'>
+            <ActiveLink to='/'
+                className='p-1'
+                activeClassName='border-b'>{t('home_page.home')}</ActiveLink>
 
-        <ActiveLink to='/about'
-            className='p-1'
-            activeClassName='border-b'>{t('home_page.about')}</ActiveLink>
+            <ActiveLink to='/about'
+                className='p-1'
+                activeClassName='border-b'>{t('home_page.about')}</ActiveLink>
+        </header>
+        <main>
+            <Outlet />
+        </main>
     </div>
 }
 function App() {
     return <div className="min-h-screen">
-        <Links />
-
         <Routes>
-            <Route
-                index
-                element={<Page element={<HomePage />} />} />
+            <Route element={<Dashboard />} >
+                <Route
+                    index
+                    element={<Page element={<HomePage />} />} />
 
-            <Route
-                path="/about"
-                element={<Page element={<AboutPage />} />} />
+                <Route
+                    path="/about"
+                    element={<Page element={<AboutPage />} />} />
+            </Route>
         </Routes>
     </div>
 }
