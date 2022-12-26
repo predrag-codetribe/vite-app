35516d0ea0121c0182781a35fc839f742a7634e1
Predrag Nikolic <predrag.nikolic@codetri.be>
2022-12-26 10:28:45 +0100

    fix: use commitlint instead of python script
    The dev might not have python installed.

---
+++ commitlint.config.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+    extends: [
+        '@commitlint/config-conventional' // scoped packages are not prefixed
+    ],
+    'type-enum': [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+    ]
+}
